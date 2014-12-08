de9fa44b5e5b01c935c529605467ddd17f0da3d7
RedAlice64 <fortestig@hotmail.com>
2014-12-08 10:13:55 +0800

    Caregiver-100

--- src/com/care/caregiver/HomeActivity.java
+++ src/com/care/caregiver/HomeActivity.java
@@ -5,11 +5,12 @@ import android.app.ProgressDialog;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
+import android.widget.*;
 import com.care.caregiver.util.Greeting;
 import com.care.caregiver.util.JsonHelper;
 import com.google.gson.Gson;
+import android.graphics.Color;
+import android.view.ViewGroup.LayoutParams;
 
 import java.io.IOException;
 import java.util.HashMap;
@@ -22,8 +23,16 @@ public class HomeActivity extends Activity {
     TextView msg;
     Button getData;
 
-    private static String domain = "http://10.0.2.2:9000/";
+    private static String domain = "http://192.168.1.110:9000/";
     private Gson gson = new Gson();
+
+    private String descriptions[][]={{"腹痛","感冒","发烧","体虚"},{"5分钟","10分钟","30分钟","1小时"},{"轻微","显著","严重","剧烈"},{"持续","阵发","阵发伴随持续","其他"}};
+    private String response[]={"你好，有哪里不舒服？","症状持续了多久？","疼痛严重吗？","症状发作时是持续还是阵发的？","用手在腹部顺时针揉动，按摩，多喝热水，或者用热水敷肚都可以缓解疼痛。"};
+    private int progress=0;
+    LinearLayout downLayout;
+
+    RadioGroup symptoms;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -32,12 +41,46 @@ public class HomeActivity extends Activity {
 
         msg = (TextView) findViewById(R.id.name);
         getData = (Button) findViewById(R.id.getdata);
-        getData.setOnClickListener(new View.OnClickListener() {
+        downLayout=(LinearLayout)findViewById(R.id.downlowinterface);
+        symptoms=new RadioGroup(this);
+        /*getData.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 new ExchangeGreeting().execute();
             }
+        });*/
+        updateView();
+        msg.setText(response[progress]);
+        getData.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                downLayout.removeAllViews();
+                msg.setText(response[++progress]);
+                if(progress<4){
+                    updateView();
+                }else{
+                    LayoutParams param=msg.getLayoutParams();
+
+                    progress=0;
+                    downLayout.removeAllViews();
+                    updateView();
+                }
+            }
         });
+    }
+
+    private void updateView(){
+        downLayout.removeAllViews();
+        symptoms.removeAllViews();
+        symptoms.setPadding(200,20,20,20);
+        for(int i=0;i<4;i++){
+            RadioButton symptom=new RadioButton(this);
+            symptom.setPadding(20,100,20,20);
+            symptom.setTextColor(Color.rgb(0,0,0));
+            symptom.setText(descriptions[progress][i]);
+            symptoms.addView(symptom);
+        }
+        downLayout.addView(symptoms);
     }
 
     private class ExchangeGreeting extends AsyncTask<String, Integer, Greeting> {
